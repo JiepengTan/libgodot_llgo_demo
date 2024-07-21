6f32fa0c290767d87fa85bee889405d026bee40e
jiepengtan <jiepengtan@gmail.com>
2024-07-21 16:36:13 +0800

    Add godot-go sample

---
+++ samples/go_sample/README.md
@@ -0,0 +1,12 @@
+# libgodot go-sample  
+
+Baisc demo from [godot-go-demo-projects](https://github.com/godot-go/godot-go-demo-projects)
+
+# How to run it
+```
+make && make run
+```
+
+# Credit
+
+* Tileset pulled from [Free RPG Asset Pack](https://biloumaster.itch.io/free-rpg-asset)
---
+++ samples/go_sample/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "C"
+import (
+	"libgodot/go_sample/scripts/demo"
+	"unsafe"
+
+	"github.com/godot-go/godot-go/pkg/core"
+	"github.com/godot-go/godot-go/pkg/ffi"
+	"github.com/godot-go/godot-go/pkg/log"
+)
+
+//export GodotGoDemo2DTopDownInit
+func GodotGoDemo2DTopDownInit(p_get_proc_address unsafe.Pointer, p_library unsafe.Pointer, r_initialization unsafe.Pointer) bool {
+	log.Debug("ExampleLibraryInit called")
+	initObj := core.NewInitObject(
+		(ffi.GDExtensionInterfaceGetProcAddress)(p_get_proc_address),
+		(ffi.GDExtensionClassLibraryPtr)(p_library),
+		(*ffi.GDExtensionInitialization)(unsafe.Pointer(r_initialization)),
+	)
+
+	initObj.RegisterSceneInitializer(func() {
+		demo.PlayerCharacterGDExtensionInit()
+		demo.RegisterClassPlayerCharacter()
+	})
+
+	initObj.RegisterSceneTerminator(func() {
+		demo.PlayerCharacterGDExtensionTerminate()
+	})
+
+	return initObj.Init()
+}
+
+func main() {
+	// log.Trace("this application is meant to be run as a plugin to godot")
+}
