baf03144a0df1c7c5e04c35bead8e8d9bbefe9c3
jiepengtan <jiepengtan@gmail.com>
2024-07-31 19:07:10 +0800

    Add script CollectableCoin

--- samples/go_sample/mario/scripts/autogen/defines.go
+++ samples/go_sample/mario/scripts/autogen/defines.go
@@ -3,7 +3,14 @@ package autogen
 
 import "C"
 import (
+	"godot-go-demo/scripts/demo"
+
+	"github.com/godot-go/godot-go/pkg/core"
 )
 
 func RegisterSceneInitializer() {
+	registerClass_CollectableCoin()
+}
+func registerClass_CollectableCoin() {
+	core.AutoRegisterClassDB[*demo.CollectableCoin]()
 }
---
+++ samples/go_sample/mario/scripts/demo/collectable_coin.go
@@ -0,0 +1,25 @@
+
+package demo
+
+import (
+    . "github.com/godot-go/godot-go/pkg/builtin"
+    . "github.com/godot-go/godot-go/pkg/gdclassimpl"
+)
+
+type CollectableCoin struct {
+    Area2DImpl
+}
+
+func (pself *CollectableCoin) V_on_body_entered(body Node2D) {
+	pself.QueueFree()
+	node := pself.GetTree().GetFirstNodeInGroup_StrExt("level_manager")
+	if(node != nil) {
+		println("V_on_body_enter find level_manager succ" )
+	} else {
+		println("V_on_body_enter can not find level_manager ")
+	}
+	println("V_on_body_entered" )
+}
+func (pself *CollectableCoin) V_ready( ) {
+	println("CollectableCoin V_ready" )
+}
